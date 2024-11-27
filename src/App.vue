e97e2a2a5467a7837ef29f5a2548217a42139392
thuanth <thuanth@rikkeisoft.com>
2024-11-27 08:33:57 +0700

    thuan dep trai

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      Thuận đẹp trai
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
